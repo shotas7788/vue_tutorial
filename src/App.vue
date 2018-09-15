a67be839957cb3c71ff4c48688556c880163e7aa
arsk78 <shotas777888@outlook.jp>
2018-09-15 16:54:02 +0900

    create cardfolder before modal

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,68 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header/>
+    <FixedTop/>
+    <router-view :cardsFolder="cardsFolder"/>
   </div>
 </template>
 
 <script>
+import 'normalize.css'
+import Header from '@/components/Header'
+import FixedTop from '@/components/FixedTop'
+import * as personalJson from '@/assets/personal-public.json'
+import * as quittingJson from '@/assets/quitting-public.json'
+import * as desireJson from '@/assets/desire-public.json'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    FixedTop
+  },
+  data(){
+    return{
+      cardFolder: null
+    }
+  },
+  mounted() {
+    this.cardFolder = {
+      personal: personalJson,
+      quitting: quittingJson,
+      desire: desireJson
+    }
+  }
 }
 </script>
 
 <style>
+html, body, #app {
+  height: 100%;
+  --color-yellow: 254, 238, 157;
+  --color-blue: 31, 118, 187;
+  --color-red: 236, 61, 66;
+}
+@keyframes on-load-visible{
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  animation: on-load-visible 1s ease;
 }
+.link{
+  text-decoration: underline;
+  color: purple;
+  cursor: pointer;
+}
+/* @media screen and (min-width: 768px) {
+} */
 </style>
---
+++ src/components/CardsFolder.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cardsFolder">
+    <h1 id="page-title">{{ pageTitle }}</h1>
+    <div
+      class="card"
+      v-for="card in cards"
+      :key="card.displayOrder"
+    >
+
+    <div class="card__title">{{ card.title }}</div>
+    <div class="card__body" v-html="card.body"></div>
+    <div class="card__expand-fake-button"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardsFolder',
+  props: [ 'cardsFolder' ],
+  computed: {
+    routeName(){
+      return this.$route.name
+    },
+    pageTitle(){
+      if (!this.cardsFolder){
+        return null
+      }
+
+      switch (this.routeName) {
+        case 'AboutMe':
+        return this.cardsFolder.personal.cardsData
+        case 'Quitting':
+        return this.cardsFolder.quitting.cardsData
+        case 'Desire':
+        return this.cardsFolder.desire.cardsData
+      }
+    }
+  }
+}
+</script>
